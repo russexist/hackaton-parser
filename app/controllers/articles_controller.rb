04537d57bd211ccb69828ac6fde51e4adf60a6a1
russexist <jambo29@yandex.com>
2018-04-14 18:22:18 +0300

    fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,17 @@
 class ArticlesController < ApplicationController
   def surfingbird
-    @first_articles = FirstArticle.all.paginate(:page => params[:page])
+    @first_articles = FirstArticle.all.paginate(page: params[:page])
   end
 
   def unn
-    @second_articles = SecondArticle.all.paginate(:page => params[:page])
+    @second_articles = SecondArticle.all.paginate(page: params[:page])
   end
 
   def articles_online
-    @third_articles = ThirdArticle.all.paginate(:page => params[:page])
+    @third_articles = ThirdArticle.all.paginate(page: params[:page])
   end
 
   def tnw
-    @tnw_articles = TnwArticle.all.paginate(:page => params[:page])
+    @tnw_articles = TnwArticle.all.paginate(page: params[:page])
   end
 end
